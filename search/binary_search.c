dc24ddaa1f7966dcdedf1ecf235966adb12edb72
fvvzhao <fvvzhao@gmail.com>
2025-03-18 20:00:30 +0800

    Create binary_search.c

---
+++ search/binary_search.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int binary_search(int arr[], int n, int target);
+
+int main() {
+  int arr[] = {5, 3, 4, 1, 2};
+  int n = sizeof(arr) / sizeof(int);
+  int number;
+  puts("请输入你想寻找的元素：");
+  scanf_s("%d", &number);
+  printf("%d的索引对应的下标为%d\n", number, binary_search(arr, n, number));
+  return 0;
+}
+
+int binary_search(int arr[], int n, int target) {
+  int left = 0;
+  int right = n - 1;
+  while (left <= right) {
+    int mid = (left + right) / 2;
+    if (arr[mid] == target) {
+      return mid;
+    }
+    else if (arr[mid] > target) {
+      right = mid - 1;
+    }
+    else {
+      left = mid + 1;
+    }
+  }
+  return -1;
+}
